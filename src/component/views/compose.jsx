cde84d693f38ee50818be9eb04122955bd9a4a39
elphie1185 <delphine_rabiller@yahoo.fr>
2021-03-14 16:44:11 +0000

    Add options to edit or delete entries

--- src/component/views/compose.jsx
+++ src/component/views/compose.jsx
@@ -5,16 +5,28 @@ function Compose(){
     return(
         <div>
             <div class="compose">
-                <h1>Compose</h1>
-                <form action="/compose" method="post">
-                    <div class="form-group">
+                <ul class="nav nav-tabs nav-justified">
+                    <li role="presentation" class="active"><a href="#">Create</a></li>
+                    <li role="presentation"><a href="#">Edit</a></li>
+                    <li role="presentation"><a href="#">Delete</a></li>
+                </ul>
+                {/* <form action="/compose" method="post">
+                    <select class="custom-select col-6" id="inlineFormCustomSelectPref">
+                        <option selected>Choose...</option>
+                        <option value="1">New Entry</option>
+                        <option value="2">Edit Entry</option>
+                        <option value="3">Delete Entry</option>
+                    </select> */}
+                    {/* <div class="form-group">
+                        <label>Date</label>
+                        <input class="form-control" type="text" name="postDate" />
                         <label>Journey</label>
                         <input class="form-control" type="text" name="postTitle" />
                         <label>Description</label>
                         <textarea class="form-control" name="postBody" rows="5" cols="30"></textarea>
-                    </div>
-                </form>
-                <button class="btn btn-primary btn-lg col-6" type="submit" name="button">Publish</button>
+                    </div> */}
+                {/* </form> */}
+                {/* <button class="btn btn-primary btn-lg col-6" type="submit" name="button">Submit</button> */}
             </div>
             <Footer />
         </div>
